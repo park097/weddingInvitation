86be1295edd1547e70835c340c8cc08c695dec4e
park097 <alslalslclq24@naver.com>
2026-01-29 22:44:00 +0900

    카카오톡기능 연결, 파이어베이스

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA-XaY3bIaNXnhZ2_Y5hq0xzkYVBl4am7M",
+  authDomain: "wedding-d0bb2.firebaseapp.com",
+  projectId: "wedding-d0bb2",
+  storageBucket: "wedding-d0bb2.firebasestorage.app",
+  messagingSenderId: "904910491817",
+  appId: "1:904910491817:web:823b3189a6547d57f019b8"
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
